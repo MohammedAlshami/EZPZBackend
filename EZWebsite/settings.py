09ddfb678b441bd87d1ce9dc0b3fc2b878480b06
MohammedAlshami <137855918+MohammedShami@users.noreply.github.com>
2024-01-14 21:16:04 +0800

    a

--- EZWebsite/settings.py
+++ EZWebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -131,3 +132,6 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 X_FRAME_OPTIONS = 'ALLOWALL'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
